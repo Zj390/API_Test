5cc05ef470eab5e8ad47e7f9a7b19576f24caf4b
从今 <2425903957@qq.com>
2026-08-26 17:15:42 +0800

    提取通用执行方法

--- testcases/test_api.py
+++ testcases/test_api.py
@@ -9,56 +9,52 @@ from commons.replace_util import ReplaceUtil
 
 class TestApi:
 
+    @staticmethod
+    def execute_case(caseinfo):
+        request_data = ReplaceUtil.replace_variables(
+            caseinfo["request"]
+        )
+
+        response = RequestUtil().send_all_request(
+            **request_data
+        )
+
+        AssertUtil.validate_response(
+            response,
+            caseinfo.get("validate")
+        )
+
+        ExtractUtil.extract_and_save(
+            response,
+            caseinfo.get("extract")
+        )
+
     # 获取access_token鉴权码接口
     @pytest.mark.smoke
     @pytest.mark.parametrize("caseinfo", read_yaml_testcase("testcases/test_get_token.yaml"))
     def test_get_token(self, caseinfo):     # autouse=False时，如此插入夹具
-        request_data = ReplaceUtil.replace_variables(caseinfo["request"])
-
-        res = RequestUtil().send_all_request(**request_data)  # 使用统一封装
-
-        AssertUtil.validate_response(res, caseinfo.get("validate"))
-
-        ExtractUtil.extract_and_save(res, caseinfo.get("extract"))
+        self.execute_case(caseinfo)
 
     # 查询标签接口
     @pytest.mark.user
     @pytest.mark.parametrize("caseinfo", read_yaml_testcase("testcases/test_select_flag.yaml"))
     def test_select_flag(self, caseinfo):
-        request_data = ReplaceUtil.replace_variables(caseinfo["request"])
-
-        res = RequestUtil().send_all_request(**request_data)
-
-        AssertUtil.validate_response(res, caseinfo.get("validate"))
+        self.execute_case(caseinfo)
 
     # 编辑标签接口
     @pytest.mark.user
     @pytest.mark.parametrize("caseinfo", read_yaml_testcase("testcases/test_edit_flag.yaml"))
     def test_edit_flag(self, caseinfo):
-        request_data = ReplaceUtil.replace_variables(caseinfo["request"])
-
-        res = RequestUtil().send_all_request(**request_data)
-
-        AssertUtil.validate_response(res, caseinfo.get("validate"))
+        self.execute_case(caseinfo)
 
     # 访问phpwind首页接口
     @pytest.mark.smoke
     @pytest.mark.parametrize("caseinfo", read_yaml_testcase("testcases/test_phpwind.yaml"))
     def test_phpwind(self, caseinfo):
-        request_data = ReplaceUtil.replace_variables(caseinfo["request"])
-
-        res = RequestUtil().send_all_request(**request_data)
-
-        AssertUtil.validate_response(res, caseinfo.get("validate"))
-
-        ExtractUtil.extract_and_save(res, caseinfo.get("extract"))
+        self.execute_case(caseinfo)
 
     # 登陆接口
     @pytest.mark.user
     @pytest.mark.parametrize("caseinfo", read_yaml_testcase("testcases/test_login.yaml"))
     def test_login(self, caseinfo):
-        request_data = ReplaceUtil.replace_variables(caseinfo["request"])
-
-        res = RequestUtil().send_all_request(**request_data)
-
-        AssertUtil.validate_response(res, caseinfo.get("validate"))
+        self.execute_case(caseinfo)
